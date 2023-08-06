7bacbe44e7ebc65c454a808c0a24199efc086b86
georgejeswin <jeswinmyladoor@gmail.com>
2023-08-06 19:03:27 +0530

    Server setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './typeorm/typeorm.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { UploadssModule } from './uploads/uploads.module';
+import { FileUploadModule } from './fileupload/fileUpload.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    UsersModule,
+    AuthModule,
+    FileUploadModule,
+    UploadssModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  const config: ConfigService = app.get(ConfigService);
+  app.setGlobalPrefix('/api');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('CSV uploader')
+    .setDescription('Dev Book API description')
+    .setVersion('1.0')
+    .addTag('devbook')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/swagger', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(process.env.PORT, () => {
+    console.log('[WEB]', config.get<string>('BASE_URL'));
+  });
 }
 bootstrap();
