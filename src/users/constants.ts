46402c97497b8c5e7d95c1a0180a373b4802507b
georgejeswin <jeswinmyladoor@gmail.com>
2023-08-07 11:57:31 +0530

    JWT updates

--- src/users/constants.ts
+++
@@ -1,5 +0,0 @@
-export const jwtConstants = {
-  secret: 'secretKey',
-  refreshSecret: 'refreshToken',
-  expiresIn: '1hrs',
-};
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { jwtConstants } from './constants';
 import { User } from 'src/typeorm/entities/user.entity';
 import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './services/users/users.service';
@@ -10,9 +9,9 @@ import { UsersService } from './services/users/users.service';
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
-      secret: jwtConstants.secret,
+      secret: process.env.JWT_ACCESS_SECRET,
       signOptions: {
-        expiresIn: jwtConstants.expiresIn,
+        expiresIn: process.env.JWT_EXPIRES_IN_HOURS,
       },
     }),
   ],
